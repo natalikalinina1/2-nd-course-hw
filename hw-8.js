945fdfa523886cc3baff0310a1fb377e688dc298
natalikalinina1 <kidmil@mail.ru>
2023-03-25 17:33:39 +0300

    исправлено js

--- hw-8.js
+++ hw-8.js
@@ -1,30 +1,20 @@
 //задание 1:
-
-function firstFunc(arr, callback){
- 
-	let array = [];
-		for(let key in arr){
-			
-			array += callback(arr[key]);
-		}
-	return `${array}`;
-	}	
-	function mult(elem){
-
-		return `${elem * elem},`;
-	}
-
-	 console.log(firstFunc([10, 20, 30], mult));
-	 
+function getResult(array, сallback) {
+	const result = array.reduce(сallback);
 	
-	 function sum(elem){
-         
-		return `${elem+elem},`;
-	}
+	console.log(result);
+}
+function mult(elem1, elem2) {
+	return elem1 * elem2;	
+}
+	function sum(elem1, elem2) {	
+	return elem1 + elem2;
+}
 
-	console.log(firstFunc([10, 20, 30], sum));
+getResult ([3, 4, 1, 9], mult); 
+getResult ([3, 4, 1, 9], sum); 
 
-	
+
 //Заданиe 2:
 const users = [
 	{name: 'Jon', age: 22},
@@ -43,60 +33,41 @@ console.log (users);
 
 
 //Задание 3:
-function each(arr, callback) {
-	let result = [];
-	
-	for (let elem of arr) {
-		result.push( callback(elem) ); 
-	}
-	
-	return result;
+
+const each = (arr, callback) => {
+    result = arr;
+    callback();
 }
-
-let result1 = each([1, '4', 9, 7], function(num) {
-	return num ** 2;
-});
-
-console.log(result1);
-
-let result2 = each([1, '4', 9, 'two'], function(num) {
-	let line = [1, '4', 9, 'two']; 
-    line = line.reverse();
-	return num = line;
-});
-console.log(result2);
-
-let result3 = each( [1, '1', false, 9, 'two'],function(num){
-	let myMass = [1, '4', false, 9, 'two'];
-
-	let result  = myMass.map(function(item) {
-	  let number = parseInt(item);
-	  num = isNaN(number)? item : number;
-	  return num;
-	});
-	
+const qwadr = [1, 4, 9, 7];
+const qwadrRes = () => {
+    result = result.map(item => item ** 2);
 	console.log(result);
-});
+}
+  
+const qwert = [1, '4', false, 9, 'two'];
+const reversArr = () => { 
+    result = result.reverse();
+    console.log(result);
+}
+const array = [1, '4', 9, 'two'];
+const toNumberArr = () => {
+    result = result.map(item =>(Number(item)));
+    result = result.filter(item => item >= 0); 
+    console.log(result);
+}
+each(qwadr,qwadrRes);
+each(array, reversArr); 
+each(qwert, toNumberArr);
 
-	
+
 //Задание 4:
-
-const timer = (dedline) => {
-let time = dedline;
-console.log (new Date());
 const idInterval = setInterval(() => {
-	time -= 1;
 	console.log (new Date());
 },3000);
-
 setTimeout(() => {
 clearInterval(idInterval);
 	console.log ('30 секунд прошло ');
-}, 30000 * dedline);
-};
-timer(1);
-
-
+}, 30000);
 
 //Задание 5:
 function calling() {
